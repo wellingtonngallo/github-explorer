ede1574de320a4f57f73f9d500fdcdf09f94f171
Wellington Gallo <wellinton.gallo77@gmail.com>
2021-03-09 16:49:14 -0300

    connecta na api do github

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,18 +1,22 @@
+import { useState, useEffect } from 'react';
 import { RepositoryItem } from "./RepositoryItem";
 
-const repository = {
-  name: 'unform',
-  description: 'Forms in react',
-  link: 'https://github.com/wellingtonngallo'
-}
+import '../styles/repositories.scss';
+
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch('http://api.github.com/orgs/rocketseat/repos')
+      .then(response => response.json())
+      .then(data => setRepositories(data));
+  }, [])
+
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
       <ul>
-        <RepositoryItem
-          repository={repository}
-        />
+        {repositories.map(repository => <RepositoryItem key={repository.name} repository={repository} />)}
       </ul>
     </section>
   )
